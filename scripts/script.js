0e0552b189b7aca6dfd473e2cc7dad04452ec793
GrapyGit <76045081+GrapyGit@users.noreply.github.com>
2022-08-10 18:05:11 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,147 +1,141 @@
-const todo = document.getElementById("todo")
-const button = document.getElementById("btn")
-const hint = document.getElementById("hint")
-const info = document.getElementById("info")
-const textTodo = document.getElementById("text")
-const content = document.getElementById("taskList")
-const search =  document.getElementById("search")
-const taskname = document.getElementById("Name")
-const progress = document.getElementById("progress")
-const btnclearsearch =  document.getElementById("clearSearch")
-let elements
-let cur = null
-let tasks = localStorage.tasks?JSON.parse(localStorage.getItem('tasks')):[];
-search.addEventListener('input', ()=>{
-  btnclearsearch.style.display="block"
+const todo = document.getElementById("todo")//блок справа(поиск, создание, задачи)
+const button = document.getElementById("btn")//кнопка изменения задач
+const hint = document.getElementById("hint")//подсказка
+const info = document.getElementById("info")//форма для ввода значений
+const textTodo = document.getElementById("text")//поле для ввода описания задачи
+const content = document.getElementById("taskList")//поле для задач
+const search =  document.getElementById("search")//поле поиска задачи
+const taskName = document.getElementById("Name")//поле для ввода наименования задачи
+const progress = document.getElementById("progress")//select выбора стадии задачи
+const btnClearSearch =  document.getElementById("clearSearch")//кнопка очистки поиска
+let elements//массив элементов(задач)
+let x = null//значение изменения ширины элемента 
+let tasks = localStorage.tasks?JSON.parse(localStorage.getItem('tasks')):[];//массив задач
+search.addEventListener('input', ()=>{//прослушиваем изменения в поле поиска
+  btnClearSearch.style.display="block"
   search.value = validate(search.value)
-  if (search.value.length==0){
+  if (search.value.length==0){//если ничего не введено, то очищаем
     clearSearch();
   }
   fillContent(search.value)
 })
-taskname.addEventListener('input', () =>{taskname.value = validate(taskname.value)})
-btnclearsearch.addEventListener('click', clearSearch)
-function clearSearch(){
+const updateLocalStorage =()=> localStorage.setItem('tasks',JSON.stringify(tasks))
+const clearSearch =()=>{//функция очистки поля поиска
   search.value = ""
   fillContent(search.value)
-  btnclearsearch.style.display="none"
+  btnClearSearch.style.display="none"
 }
-function updateLocalStorage(){
-  localStorage.setItem('tasks',JSON.stringify(tasks))
-}
-function filterTask(){
-  let anticipatedTask = tasks.length && tasks.filter((item)=>item.color=="var(--anticipated-color)")
-  let progressTask = tasks.length && tasks.filter((item)=>item.color=="var(--progress-color)")
-  let completedTask = tasks.length && tasks.filter((item)=>item.color=="var(--completed-color)")
+const creatTodo =(task,index,visible)=>{//создаём шаблон задачи
+  return `<div class="task" style="display:${visible?"flex":"none"}; ${task.active?"background:linear-gradient(90deg, var(--borderbtnColor) 0%,  var( --setting-bg-color) 100%);padding-left:40px;":""}">
+           <img src="img/delete.png" onclick="deleteTask(${index})"><p style="color:${task.color}" onclick="redactListTodo(${index})">${task.name}</p>
+         </div>`
+ }
+const filterTask =()=>{//фильтруем задачи(сначала идут в процессе, потом в ожидании, а следом выполненые)
+  anticipatedTask = tasks.length && tasks.filter((item)=>item.color=="var(--anticipated-color)")
+  progressTask = tasks.length && tasks.filter((item)=>item.color=="var(--progress-color)")
+  completedTask = tasks.length && tasks.filter((item)=>item.color=="var(--completed-color)")
   tasks=[...progressTask,...anticipatedTask,...completedTask]
 }
-function fillContent(search_text){
+const fillContent = search_text=>{//заполняем список задач
   content.innerHTML=""
-  let visible=1;
+  let visible=1;//состояние видимости задачи
   if (tasks.length>0){
-     filterTask()
+    filterTask()//фильтруем задачи
     tasks.forEach((item,index)=>{
-    if(search_text && item.name.search(search_text)==-1) visible=0
-      content.innerHTML+=creatTodo(item,index,visible)
+    if(search_text && item.name.search(search_text)==-1) visible=0//проверяем на совпадение с поиском(если совпадает, то выводим)
+      content.innerHTML+=creatTodo(item,index,visible)//добавляем задачу в список
       visible=1
     })
   }
-  elements = document.querySelectorAll(".task")
+  elements = document.querySelectorAll(".task")//получаем список задач(html)
 }
-fillContent()
-function validate(str){
-  str = str.replace(/^\s+/g,'');
-  str = str.replace(/[ ]{1,}/g,' ');
+taskName.addEventListener('input', () =>{taskName.value = validate(taskName.value)})//прослушиваем изменения поля ввода наименования задачи и делаем валидацию
+btnClearSearch.addEventListener('click', clearSearch)//Добавляем функцию очистки поиска при клике на кнопку(img)
+const validate=str=>{//проверяем строку
+  str = str.replace(/^\s+/g,'');//очищаем, если введён только пробел
+  str = str.replace(/[ ]{1,}/g,' ');//очищаем двойные пробелы
   return str
 }
-function creatTodo(task,index,visible){
- return `<div class="task" style="display:${visible?"flex":"none"}">
-          <img src="img/delete.png" onclick="deleteTask(${index})"><p style="color:${task.color}" onclick="redacListTodo(${index})">${task.name}</p>
-        </div>`
-}
-function deleteTask(index){
-  if (elements[index].style.background == "var(--main-color)"){ clearInfo("")
-}
+const deleteTask=index=>{//удаляем задачу
+  if (tasks[index].active) settingInfo("")
+  //дальше идёт подготовка к анимации удаления
   elements[index].style.height = `${elements[index].scrollHeight}px`
   window.getComputedStyle(elements[index],null).getPropertyValue("height")
   elements[index].style.height = "0"
   tasks.splice(index,1)
- setTimeout(()=>{
+ setTimeout(()=>{//делаем задержку в выполнении, чтобы успела проиграться анимация удаления
   updateLocalStorage()
   fillContent(search.value)
  },500)
 }
-function redacListTodo(index){
-  fillContent(search.value)
-  info.style.display = "grid"
-  hint.style.display = "none"
-  if (index==undefined || index==null){
-    button.innerHTML = "Добавить"
-    taskname.value = ""
-    textTodo.value=""
-    progress.selectedIndex =0
-  }
-  if (index>=0){
-    elements[index].style.background="var(--main-color)"
-    taskname.value = tasks[index].name
-    textTodo.value = tasks[index].text
-    progress.value = tasks[index].color
-    button.innerHTML="Изменить"
-  }
-    button.setAttribute('onclick','redact('+index+')');
+const redactListTodo=index=>{
+  if (index==undefined || index==null) settingInfo("Добавить")//если индекса нет, то подготавливаем форму для добавления задачи
+  if (index>=0) settingInfo("Изменить",tasks[index]);//если индекс есть, то подготавливаем форму для изменения задачи
+    button.setAttribute('onclick','redacTasks('+index+')');//изменяем функцию клика кнопки(нужно, чтобы передать индекс задачи)
 }
-function redact(index){
-  if (taskname.value.length!=0){
-  let task = {
-  name : taskname.value,
-  text: textTodo.value,
-  color : progress.value
+const redacTasks =index=>{
+  if (taskName.value.length==0){//проверяем, что ввели наименование задачи
+    alert("Введите имя задачи!")
+    return;
   }
-  if (index!=undefined || index!=null){
+  let task = {//создаём задачу
+    name : taskName.value,//читаем значение наименования
+    text: textTodo.value,//читаем значение описания
+    active:false,//делаем неактивной задачу
+    color : progress.value//читаем цвет(так же является степень выполнения задачи)
+  }
+  if (index!=undefined || index!=null)//если индекс введён, то присваиваем задаче новое значение
     tasks[index] = task
+  else tasks.push(task)//если индекс не введён, то добавляем в конец массива задач
+  settingInfo("")//очищаем форму ввода значений
+  updateLocalStorage()//обновляем локальное хранилище
+  fillContent(search.value)//обновляем список задач(html)
+}
+function settingInfo(str, task) {
+  tasks.forEach((item) => {
+    item.active = false
+  })
+  info.style.display = "grid"//делаем формы для заполнения видимыми
+  hint.style.display = "none"//скрываем задачу
+  switch (str) {
+    case 'Добавить'://очищаем формы для поиска
+      progress.selectedIndex = 0
+      taskName.value = ""
+      textTodo.value = ""
+      button.innerHTML = str
+      break
+    case 'Изменить':
+      with (task) {//заполняем формы для ввода, значениями выбранной задачи
+        active = true//делаем задачу активной
+        taskName.value = name
+        textTodo.value = text
+        progress.value = color
+      }
+      break
+    default:
+      info.style.display = "none"//скрываем формы для заполнения
+      hint.style.display = "block"//делаем подсказку видимой
+      break
   }
-  else tasks.push(task)
-
-  clearInfo("")
-  updateLocalStorage()
-  fillContent(search.value)
+  fillContent(search.value)//обновляем список задач(html, нужно для того, чтобы добавить или снять выделение с задачи)
 }
-else alert("Введите имя задачи!")
+const hook =e=> {//функция работает при нвжатии на элемент границы
+  e = e || window.event;//получаем обьект события
+  x =  e.clientX - todo.offsetWidth
 }
-function clearInfo(str){
- fillContent(search.value)
-  info.style.display = "none"
-  hint.style.display = "block"
-  progress.selectedIndex =0
-  taskname.value =""
-  textTodo.value =""
-  button.innerHTML=str
+const unhook=e=> {
+  if(x)
+    x = null;//если кнопка отжата, обнуляем значение
 }
-//потом разобраться
-function hook(e) {
-  e = e || window.event;
-  cur = { 'el': todo, 'x': e.clientX - todo.offsetWidth, 'y': e.clientY - todo.offsetHeight }
-}
-
-function unhook(e) {
-  if( cur )
-    cur = null;
-}
-function move(e) {
-  if( !cur )
+const move=e=> {
+  if(!x)
     return;
   e = e || window.event;
-    with( cur )
-    {
-      var nx = e.clientX - x;
-      if( nx < 40 ) nx = 40;
-      todo.style.width = nx + 'px';
-  }
+  todo.style.width = e.clientX - x + 'px';
   (e.preventDefault) ? e.preventDefault() : e.returnValue = false;
 }
-document.onmouseup = unhook;
-document.onmousemove = move;
-document.ondragstart = function()
-{
-    return false;
-}
+document.onmouseup = unhook;//отжатие кнопки мыши
+document.onmousemove = move;//движение мыши
+document.ondragstart = ()=>{return false}//убираем стандартное поведение браузера при перетаскивании злементов
+fillContent()
+//the end
